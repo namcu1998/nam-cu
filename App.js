3ba79fc41a2142a134dfe1980986de2bbabe772f
Namcu1998 <namcu1998@gmail.com>
2022-04-16 16:12:48 +0700

    nam

--- App.js
+++ App.js
@@ -6,75 +6,61 @@ import { images, icons, FONTS, COLORS, SIZES, lottiefiles } from "./constants";
 
 LogBox.ignoreLogs(["Setting a timer"]);
 
-const socket = io("https://nam-cu.herokuapp.com/web_app");
-
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      sensor_board_information: {},
-      sensor_data: {
-        temp_value: 100,
-        humi_value: 100,
-      },
+      loading: true,
+      data: {},
     };
   }
 
   componentDidMount() {
-    // socket.on("send_all_data", (data) => {
-    //   console.log("data: ", data);
-    //   this.setState({
-    //     sensor_board_information: { ...data },
-    //   });
-    // });
+    const socket = io("http://192.168.1.5:3000/web_app");
 
-    // socket.on("temp_value", (value) => {
-    //   const data = { ...this.state.sensor_data };
-
-    //   data.temp_value = value;
-
-    //   this.setState({
-    //     sensor_data: { ...data },
-    //   });
-    // });
-
-    // socket.on("humi_value", (value) => {
-    //   const data = { ...this.state.sensor_data };
-
-    //   data.humi_value = value;
-
-    //   this.setState({
-    //     sensor_data: { ...data },
-    //   });
-    // });
+    socket.on("send_all_data", (data) => {
+      this.setState({
+        loading: false,
+        data: data,
+      });
+    });
   }
 
   render() {
-    console.log(this.state.sensor_board_information);
+    const show_screen = () => {
+      const { connected, sensor, sensor_board_information } =
+        this.state.data.sensor_board;
 
-    const { sensor_board_information } = this.state;
-    return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <View style={styles.status}>
-            <Text style={{ fontSize: 15, fontWeight: "bold" }}>ESP: </Text>
-            <Text style={{ fontSize: 15, fontWeight: "bold" }}>
-              {sensor_board_information.sensor_board_status
-                ? "Connected"
-                : "Disconnected"}
-            </Text>
+      console.log(
+        "-----------------------------------------------------------------------------------------"
+      );
+      return (
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <View style={styles.status}>
+              <Text style={{ fontSize: 15, fontWeight: "bold" }}>ESP: </Text>
+              <Text style={{ fontSize: 15, fontWeight: "bold" }}>
+                {connected ? "Connected" : "Disconnected"}
+              </Text>
+            </View>
+            <View>
+              <Image
+                source={icons.ip}
+                resizeMode="cover"
+                style={{ width: 20, height: 20 }}
+              />
+            </View>
           </View>
-          <View></View>
+          <Text></Text>
         </View>
-        <Text>{sensor_board_information.ramLeft}</Text>
-        <Image
-          source={icons.ip}
-          resizeMode="cover"
-          style={{
-          }}
-        />
-      </View>
-    );
+      );
+    };
+
+    const show_loading_screen = () => {
+      return <View style={styles.container}></View>;
+    };
+
+    return this.state.loading ? show_loading_screen() : show_screen();
   }
 }
 
@@ -82,11 +68,12 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 30,
     marginHorizontal: 10,
+
   },
   header: {
     height: 30,
     backgroundColor: "red",
-    justifyContent: "center",
+    flexDirection: 'row',
   },
   status: {
     flexDirection: "row",
