25c0c0773566449e322745ef1e5465bb30f20480
namcu1998 <namcu1998@gmail.com>
2022-05-01 19:13:10 +0700

    nam

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,24 +1,122 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
 import React, { Component } from "react";
 import { Header } from "@rneui/themed";
 import { LinearGradient } from 'expo-linear-gradient';
+import { icons } from "../constants";
 
 export class HomeScreen extends Component {
   render() {
     return (
-      <View>
-        <Header
-          ViewComponent={LinearGradient} // Don't forget this!
-          linearGradientProps={{
-            colors: ["red", "pink"],
-            start: { x: 0, y: 0.5 },
-            end: { x: 1, y: 0.5 },
-          }}
-        />
-        <Text>HomeScreen</Text>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <LinearGradient
+            colors={['#1A73E9', '#6C92F4']}
+            start={{ x: 0.5, y:0.0 }}
+            style={styles.background_icon}
+          >
+          <TouchableOpacity 
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              width: '100%',
+              height: '100%',
+              paddingLeft: 20,
+              paddingRight: 10
+            }}
+            onPress={() => {this.props.navigation.navigate('Register')}}
+          >
+            <Text style={{color: 'white', fontWeight: 'bold', fontSize: 14}}>ADD</Text>
+            <Image 
+              source={icons.add_icon}
+              style={styles.icon}
+            />
+            </TouchableOpacity>
+          </LinearGradient>
+        </View>
+        <View style={styles.body}>
+          <Text style={{
+            fontSize: 30,
+            fontWeight: 'bold'
+          }}>HomeScreen</Text>
+          <View style={styles.card}>
+            <View style={styles.view_left}>
+              <View style={styles.cycle}>
+              </View>
+             <Text style={{fontSize: 16, fontWeight: 'bold', marginLeft: 8}}>Room 1</Text>
+            </View>
+            <View style={styles.view_right}>
+              <Image
+                source={icons.right_arrow}
+                style={{
+                  resizeMode: 'contain',
+                  width: 18,
+                  height: 18,
+                  tintColor: '#00000080'
+                }}
+              />
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    width: '100%',
+    paddingTop: 50,
+    paddingHorizontal: 30,
+    backgroundColor: '#FEEBEB'
+  },
+  icon: {
+    width: 25,
+    height: 25,
+    resizeMode: "contain",
+    tintColor: "#fff"
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  },
+  background_icon: {
+    width: 97,
+    height: 40,
+    flexDirection: 'row',
+    backgroundColor: 'green',
+    borderRadius: 20
+  },
+  body: {
+    marginTop: 20,
+    alignItems: 'center'
+  },
+  card: {
+    width: '100%',
+    height: 52,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: '#fff',
+    borderRadius: 15,
+    paddingHorizontal: 10
+  },
+  view_left: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  cycle: {
+    width: 10,
+    height: 10,
+    backgroundColor: '#3312FF',
+    borderRadius: 20,
+  },
+  view_right: {
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    //backgroundColor: 'blue'
+  }
+})
 
 export default HomeScreen;
--- screen/RegScreen.js
+++ screen/RegScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 class RegScreen extends Component {
   constructor(props) {
@@ -10,11 +10,62 @@ class RegScreen extends Component {
 
   render() {
     return (
-      <View>
-        <Text> RegScreen </Text>
+      <View style={styles.container}>
+        <Text style={{fontSize: 20, fontWeight: 'bold'}}> Register </Text>
+        <View style={styles.input_container}>
+          <Text style={{fontSize: 20, fontWeight: 'bold'}}>Board name</Text>
+          <TextInput placeholder='Board name' style={styles.input} />
+        </View>
+        <View style={styles.input_container}> 
+          <Text style={{fontSize: 20, fontWeight: 'bold'}}>Board space name</Text>
+          <TextInput placeholder='Board space name' style={styles.input} />
+        </View>
+        <LinearGradient
+          colors={['#92c25770', '#4ea0f2']}
+          start={{x:1, y:0.5}}
+          style={styles.button}
+        >
+          <TouchableOpacity
+            style={{
+              width: '100%',
+              height: '100%',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            <Text
+              style={{fontWeight: 'bold'}}
+            >CREATE</Text>
+          </TouchableOpacity>
+        </LinearGradient>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+    paddingHorizontal: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  input: {
+    width: '100%',
+    height: 30,
+    borderBottomWidth: 1,
+    borderColor: '#00000070'
+  },
+  input_container: {
+    width: '100%',
+    marginTop: 10
+  },
+  button:{
+    width: '100%',
+    height: 50,
+    borderRadius: 25,
+    marginTop: 10
+  }
+})
 
 export default RegScreen;
